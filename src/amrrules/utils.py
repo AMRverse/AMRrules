4585a4270f0c9760da3ae53791bfc70d7eb6fc28
Jane Hawkey <5591817+jhawkey@users.noreply.github.com>
2026-01-19 11:54:12 +1100

    fixed stop codons in aa conversion

--- src/amrrules/utils.py
+++ src/amrrules/utils.py
@@ -4,7 +4,7 @@ import warnings
 
 aa_conversion = {'G': 'Gly', 'A': 'Ala', 'S': 'Ser', 'P': 'Pro', 'T': 'Thr', 'C': 'Cys', 'V': 'Val', 'L': 'Leu', 'I': 'Ile', 
                  'M': 'Met', 'N': 'Asn', 'Q': 'Gln', 'K': 'Lys', 'R': 'Arg', 'H': 'His', 'D': 'Asp', 'E': 'Glu', 'W': 'Trp', 
-                 'Y': 'Tyr', 'F': 'Phe', 'Ter': 'STOP'}
+                 'Y': 'Tyr', 'F': 'Phe', 'STOP': 'Ter', '*': 'Ter'}
 
 required_cols = ['variation type', 'gene', 'mutation']
 minimal_columns = ['ruleID', 'gene context', 'drug', 'drug class', 'phenotype', 'clinical category', 'evidence grade', 'version', 'organism']
